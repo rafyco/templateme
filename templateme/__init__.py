e83766e3d2d54c333804a182dcf6d95b234097e1
GitHub Action <action@github.com>
2019-12-17 08:05:53 +0000

    Upgrade to 0.0.7 version

--- templateme/__init__.py
+++ templateme/__init__.py
@@ -6,6 +6,6 @@ Template Me - module with template for your favourite projects.
 
 def get_version():
     """ Get version of protemp package. """
-    return "0.0.6"
+    return "0.0.7"
 
 __version__ = get_version()
